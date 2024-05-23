0a4659ad810351bb39308950c69c2c2eef068ea6
Beniih <albenir1989@gmail.com>
2024-05-23 12:41:49 -0300

    fix: aura audio fx

--- elements/misc/aura.gd
+++ elements/misc/aura.gd
@@ -7,10 +7,10 @@ extends Node2D
 func aura_damage():
 	var bodies = aura_collision.get_overlapping_bodies()
 	for body in bodies:
-		slap.play(.2)
 		if !is_instance_valid(body):
 			return
 		if body.is_in_group("enemy"):
+			slap.play(.2)
 			var nivel: int = GameManager.nivel
 			var dmg: int = 1 + floori(nivel / 2)
 			body.take_damage(dmg, true)
